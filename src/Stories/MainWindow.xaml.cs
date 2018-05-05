04c10bb0dcaa061625282e1f3f1d8c57aa78b6e7
Bartłomiej Teodorczuk <deremaki@hotmail.com>
2018-05-05 21:19:09 +0200

    fixy + dummy query runner in gui

--- src/Stories/MainWindow.xaml.cs
+++ src/Stories/MainWindow.xaml.cs
@@ -11,5 +11,14 @@
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            //na odpierdol sie, do wyjebania potem - i tak trzeba zrobić do tego jakies lepsze gui
+            var query = Parser.Parsing.GetQuery(QueryTextBox.Text);
+            var serialized = Newtonsoft.Json.JsonConvert.SerializeObject(
+                new { query }, Newtonsoft.Json.Formatting.Indented);
+            System.Windows.MessageBox.Show(serialized);
+        }
     }
 }
