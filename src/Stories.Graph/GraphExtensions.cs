b620cf4d559831365db5a62bd14134bd02d8f856
pienkowskii <pienkowski.igor@o2.pl>
2018-05-06 21:08:36 +0200

    rename

--- src/Stories.Graph/GraphExtensions.cs
+++ src/Stories.Graph/GraphExtensions.cs
@@ -10,25 +10,25 @@ namespace Stories.Graph
     {
         public static IEnumerable<Vertex> FindVerticesSatisfyingCondition(this Graph graph, ConditionExpression condition)
         {
-            var fluents = extractFluentsValues(condition);
+            var fluents = ExtractFluentsValues(condition);
             return graph.Vertexes.Where(v => fluents.All(f => v.State.Values[f.Key] == f.Value));
         }
 
         public static IEnumerable<Vertex> FindVerticesSatisfyingCondition(this IEnumerable<Vertex> vertices, ConditionExpression condition)
         {
-            var fluents = extractFluentsValues(condition);
+            var fluents = ExtractFluentsValues(condition);
             return vertices.Where(v => fluents.All(f => v.State.Values[f.Key] == f.Value));
         }
 
-        private static IEnumerable<KeyValuePair<string, bool>> extractFluentsValues(ConditionExpression condition)
+        private static IEnumerable<KeyValuePair<string, bool>> ExtractFluentsValues(ConditionExpression condition)
         {
             switch (condition)
             {
                 case ConditionConstant constant: return new Dictionary<string, bool>();
-                case ConditionNegation negation: return extractFluentsValues(negation.Expression).Select(x => new KeyValuePair<string, bool>(x.Key, !x.Value));
+                case ConditionNegation negation: return ExtractFluentsValues(negation.Expression).Select(x => new KeyValuePair<string, bool>(x.Key, !x.Value));
                 case ConditionOperation operation:
-                    var left = extractFluentsValues(operation.Left);
-                    var right = extractFluentsValues(operation.Right);
+                    var left = ExtractFluentsValues(operation.Left);
+                    var right = ExtractFluentsValues(operation.Right);
 
                     return left.Concat(right);
                 case ConditionVariable variable: return new Dictionary<string, bool> { { variable.Label, true } };
