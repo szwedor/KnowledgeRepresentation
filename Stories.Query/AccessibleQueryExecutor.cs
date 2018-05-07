7dbc509a7451c470207c0dc851f304442edc075d
pienkowskii <pienkowski.igor@o2.pl>
2018-05-07 21:36:38 +0200

    necessary accessible

--- Stories.Query/AccessibleQueryExecutor.cs
+++ Stories.Query/AccessibleQueryExecutor.cs
@@ -1,13 +1,10 @@
 ﻿using Stories.Graph;
 using Stories.Graph.Model;
-using Stories.Parser.Conditions;
 using Stories.Parser.Statements;
 using Stories.Parser.Statements.QueryStatements;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Stories.Query
 {
@@ -30,7 +27,7 @@ namespace Stories.Query
                 case Sufficiency.Possibly:
                     break;
                 case Sufficiency.Typically:
-                    break;
+                    return ExecuteTypicallySufficiency(startVertices, endVertices);
                 default:
                     throw new InvalidOperationException();
             }
@@ -39,21 +36,76 @@ namespace Stories.Query
 
         private static bool ExecuteNecessarySufficiency(IEnumerable<Vertex> startVertices, IEnumerable<Vertex> endVertices)
         {
-            var queryResult = true;
-            var verticesToCheck = startVertices;
-            var closedVertices = new SortedSet<Vertex>(Comparer<Vertex>.Create((x, y) => x.Equals(y) ? 0 : 1));
-            do
+            // TODO do uwzględnienia zdania  "y after xyz"
+            if (endVertices.Count() == 0 || startVertices.Count() == 0)
             {
-                if (!verticesToCheck.All(v => endVertices.Contains(v)))
+                return false;
+            }
+
+            var queryResult = true;
+            int verticesCompleted = 0;
+
+            foreach (var sVertex in startVertices)
+            {
+                var closedVertices = new SortedSet<Vertex>(Comparer<Vertex>.Create((x, y) => x.Equals(y) ? 0 : 1));
+                var verticesToCheck = new List<List<Vertex>>() { new List<Vertex> { sVertex } };
+
+                do
                 {
-                    queryResult = false;
-                    break;
-                }
-                closedVertices.UnionWith(verticesToCheck);
-                verticesToCheck = verticesToCheck.SelectMany(x => x.EdgesOutgoing.Select(y => y.To)).Distinct().Except(closedVertices).ToList();
-            } while (!verticesToCheck.All(v => closedVertices.Contains(v)));
+                    // po kolei sprawdzamy grupy wierzchołków, do których prowadzi (aktor, akcja)
+                    // jeśli dla jakiejś grupy wierzchołków, do której prowadzi (aktor, akcja) wszystkie stany spełniają warunek końcowy to wejdź w if'a
+                    if (verticesToCheck.Any(x => x.All(y => endVertices.Contains(y))))
+                    {
+                        verticesCompleted++;
+                        break;
+                    }
+                    
+                    closedVertices.UnionWith(verticesToCheck.SelectMany(x => x).Distinct());
+
+                    // grupujemy krawędzie po aktorach i akcjach
+                    var edgesGroupedByActorAction = verticesToCheck.SelectMany(x => x).Distinct().SelectMany(x => x.EdgesOutgoing).GroupBy(y => new { y.Action, y.Actor }).ToList();
+                    // jedna pozycja w liście to zbiór wierzchołków, do których możemy się dostać po wykonaniu (aktor, akcja)
+                    verticesToCheck = edgesGroupedByActorAction.Select(x => x.Select(y=>y.To).ToList()).ToList();
+
+                    // sprawdzamy, czy wszystkie wierzchołki do rozważenia były już rozważane
+                } while (!verticesToCheck.SelectMany(x => x.ToList()).All(v => closedVertices.Contains(v)));
+            }
+
+            if (verticesCompleted != startVertices.Count())
+            {
+                queryResult = false;
+            }
 
             return queryResult;
+        }
+
+        private static bool ExecuteTypicallySufficiency(IEnumerable<Vertex> startVertices, IEnumerable<Vertex> endVertices)
+        {
+            throw new NotImplementedException();
+            //var queryResult = true;
+            //var verticesToCheck = startVertices;
+            //var closedVertices = new SortedSet<Vertex>(Comparer<Vertex>.Create((x, y) => x.Equals(y) ? 0 : 1));
+            //do
+            //{
+            //    if (!verticesToCheck.Any(v => endVertices.Contains(v)))
+            //    {
+            //        queryResult = false;
+            //        break;
+            //    }
+            //    closedVertices.UnionWith(verticesToCheck);
+            //    var typicalVertices = verticesToCheck.SelectMany(x => x.EdgesOutgoing.Where(y => y.IsTypical).Select(y => y.To)).Distinct().ToList();
+            //    if (typicalVertices.Count > 0)
+            //    {
+            //        verticesToCheck = typicalVertices.Except(closedVertices).ToList();
+            //    }
+            //    else
+            //    {
+            //        verticesToCheck = verticesToCheck.SelectMany(x => x.EdgesOutgoing.Select(y => y.To)).Distinct().Except(closedVertices).ToList();
+            //    }
+
+            //} while (!verticesToCheck.All(v => closedVertices.Contains(v)));
+
+            //return queryResult;
         }
     }
 }
